b7aec8ffd5dacb4b75fc01b0bcf6bfefb2e3d92c
visitkoushik <visit.koushik@gmail.com>
2022-12-03 14:47:21 +0530

    Implemented the of a layout

--- src/App.tsx
+++ src/App.tsx
@@ -246,6 +246,21 @@ function App() {
       let sitNav = convertToSiteNavigation(json);
       console.log(sitNav);
       JointService.layout(tree, paperScroller);
+
+
+      let filename = "saved.json";
+        let text = JSON.stringify(sitNav);
+        let blob = new Blob([text], {type:'text/plain'});
+        let link = document.createElement("a");
+        link.download = filename;
+        //link.innerHTML = "Download File";
+        link.href = window.URL.createObjectURL(blob);
+        document.body.appendChild(link);
+        link.click();
+        setTimeout(() => {
+            document.body.removeChild(link);
+            window.URL.revokeObjectURL(link.href);
+        }, 100);
     };
 
     graph.on("change", function (cell, opt) {
